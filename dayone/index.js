c18a9ebe05a953df109f68d6a8e7c369b359a014
Ishaku Dyelshak <ishakudyelshak@gmail.com>
2025-01-13 23:04:51 +0100

    Add files via upload

---
+++ dayone/index.js
@@ -0,0 +1,13 @@
+let squad = ["jex", "mex","phils", "tz","Shyar"];
+let text ="";
+for(let i=0; i<squad.length;i++){
+    text += squad[i] + "<br>";
+    console.log(text[i])}
+
+const cars = ["BMW", "Volvo","racy", "Saab", "Ford", "Fiat", "Audi"];
+
+ text = "";
+for (let i = 2; i < cars.length; i++) {
+  text += cars[i] + "<br>";
+  console.log(cars)
+}
